5fbb6edec5e670acdbde7a6e7b6e3af370a87330
Fola <folajimibabslawal@gmail.com>
2025-11-09 20:14:48 +0100

    6

---
+++ FeetToMeters.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class FeetToMeters {
+
+public static void main (String[]args) {
+
+Scanner input = new Scanner(System.in);
+
+System.out.print("Enter a value in feet: ");
+double feet = input.nextDouble();
+
+double meter = feet * 0.305;
+
+System.out.println(feet + " feet is " + meter + " meters.");
+
+}
+
+}
+
